5d63c4e85339e21f867c2576bcf2c6832a3068b3
Pedro M <pedromoreirareis@gmail.com>
2017-03-16 18:12:42 -0300

    Troca do findViewById pela biblioteca Data Binding, melhora da implementção do algoritmo.

--- app/src/main/java/com/pedromoreirareisgmail/capitaisdomundo/MainActivity.java
+++ app/src/main/java/com/pedromoreirareisgmail/capitaisdomundo/MainActivity.java
@@ -3,6 +3,7 @@ package com.pedromoreirareisgmail.capitaisdomundo;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
@@ -10,18 +11,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 
+import com.pedromoreirareisgmail.capitaisdomundo.databinding.ActivityMainBinding;
+
 import java.text.NumberFormat;
+
 
 public class MainActivity extends Activity {
 
     private static final NumberFormat PERCENT_FORMAT = NumberFormat.getPercentInstance();
     private static final int QUANT_PERGUNTAS = 10;
+    private ActivityMainBinding binding;
     private int mCerto = 0;
-
-    private Button.OnClickListener clickButConferir = new Button.OnClickListener() {
+    private final Button.OnClickListener clickButConferir = new Button.OnClickListener() {
         @Override
         public void onClick(View v) {
             conferirEdits();
@@ -35,10 +38,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //SetContentView para Data Binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        Button butConferir = (Button) findViewById(R.id.but_conferir);
-        butConferir.setOnClickListener(clickButConferir);
+        binding.butConferir.setOnClickListener(clickButConferir);
     }
 
     private String gerarMensagemResposta() {
@@ -47,7 +50,7 @@ public class MainActivity extends Activity {
         totalDisplay = getResources().getString(R.string.msgRespostas) + "  " + String.valueOf(mCerto)
                 + getResources().getString(R.string.msgBarra) + QUANT_PERGUNTAS +
                 "\n" +
-                "\n" + PERCENT_FORMAT.format(mCerto / QUANT_PERGUNTAS) +
+                "\n" + PERCENT_FORMAT.format(mCerto / Double.valueOf(QUANT_PERGUNTAS)) +
                 "  " + getResources().getString(R.string.msgDoTotal);
 
         return totalDisplay;
@@ -64,6 +67,13 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 zerarRespostas();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                binding.includedEdit.noruegaResp.requestFocus();
             }
         });
         alerta.create().show();
@@ -73,10 +83,7 @@ public class MainActivity extends Activity {
         String respCerta;
         String respUser;
 
-        EditText textoNoruega = (EditText) findViewById(R.id.noruega_Resp);
-        textoNoruega.requestFocus();
-
-        respUser = textoNoruega.getText().toString().toUpperCase().trim();
+        respUser = binding.includedEdit.noruegaResp.getText().toString().toUpperCase().trim();
         if (!respUser.equals("")) {
             respCerta = getResources().getString(R.string.noruegaRespCerta).toUpperCase();
 
@@ -85,9 +92,7 @@ public class MainActivity extends Activity {
             }
         }
 
-        EditText textoSuecia = (EditText) findViewById(R.id.suecia_Resp);
-
-        respUser = textoSuecia.getText().toString().toUpperCase().trim();
+        respUser = binding.includedEdit.sueciaResp.getText().toString().toUpperCase().trim();
         if (!respUser.equals("")) {
             respCerta = getResources().getString(R.string.sueciaRespCerta).toUpperCase();
 
@@ -99,59 +104,42 @@ public class MainActivity extends Activity {
 
     private void conferirRadios() {
 
-        RadioGroup emiradoArabes = (RadioGroup) findViewById(R.id.emiradosArabes_RadioGroup);
-        if (emiradoArabes.getCheckedRadioButtonId() == R.id.emiradosArabes_RespC) {
+        if (binding.includedRadio.emiradosArabesRadioGroup.getCheckedRadioButtonId() == R.id.emiradosArabes_RespC) {
             mCerto++;
         }
 
-        RadioGroup australia = (RadioGroup) findViewById(R.id.australia_RadioGroup);
-        if (australia.getCheckedRadioButtonId() == R.id.australia_RespE) {
+        if (binding.includedRadio.australiaRadioGroup.getCheckedRadioButtonId() == R.id.australia_RespE) {
             mCerto++;
         }
 
-        RadioGroup turquia = (RadioGroup) findViewById(R.id.turquia_RadioGroup);
-        if (turquia.getCheckedRadioButtonId() == R.id.turquia_RespB) {
+        if (binding.includedRadio.turquiaRadioGroup.getCheckedRadioButtonId() == R.id.turquia_RespB) {
             mCerto++;
         }
 
-        RadioGroup canada = (RadioGroup) findViewById(R.id.canada_RadioGroup);
-        if (canada.getCheckedRadioButtonId() == R.id.canada_RespB) {
+        if (binding.includedRadio.canadaRadioGroup.getCheckedRadioButtonId() == R.id.canada_RespB) {
             mCerto++;
         }
 
-        RadioGroup belgica = (RadioGroup) findViewById(R.id.belgica_RadioGroup);
-        if (belgica.getCheckedRadioButtonId() == R.id.belgica_RespA) {
+        if (binding.includedRadio.belgicaRadioGroup.getCheckedRadioButtonId() == R.id.belgica_RespA) {
             mCerto++;
         }
 
-        RadioGroup franca = (RadioGroup) findViewById(R.id.franca_RadioGroup);
-        if (franca.getCheckedRadioButtonId() == R.id.franca_RespD) {
+        if (binding.includedRadio.francaRadioGroup.getCheckedRadioButtonId() == R.id.franca_RespD) {
             mCerto++;
         }
     }
 
     private void conferirCheck() {
-        CheckBox africaDoSulA = (CheckBox) findViewById(R.id.africaDoSul_RespA);
-        CheckBox africaDoSulB = (CheckBox) findViewById(R.id.africaDoSul_RespB);
-        CheckBox africaDoSulC = (CheckBox) findViewById(R.id.africaDoSul_RespC);
-        CheckBox africaDoSulD = (CheckBox) findViewById(R.id.africaDoSul_RespD);
-        CheckBox africaDoSulE = (CheckBox) findViewById(R.id.africaDoSul_RespE);
 
-        CheckBox georgiaA = (CheckBox) findViewById(R.id.georgia_RespA);
-        CheckBox georgiaB = (CheckBox) findViewById(R.id.georgia_RespB);
-        CheckBox georgiaC = (CheckBox) findViewById(R.id.georgia_RespC);
-        CheckBox georgiaD = (CheckBox) findViewById(R.id.georgia_RespD);
-        CheckBox georgiaE = (CheckBox) findViewById(R.id.georgia_RespE);
-
-        if (africaDoSulA.isChecked() && africaDoSulB.isChecked() &&
-                africaDoSulE.isChecked() && !africaDoSulC.isChecked() &&
-                !africaDoSulD.isChecked()) {
+        if (binding.includedCheck.africaDoSulRespA.isChecked() && binding.includedCheck.africaDoSulRespB.isChecked() &&
+                binding.includedCheck.africaDoSulRespE.isChecked() && !binding.includedCheck.africaDoSulRespC.isChecked() &&
+                !binding.includedCheck.africaDoSulRespD.isChecked()) {
             mCerto++;
         }
 
-        if (georgiaC.isChecked() && georgiaE.isChecked() &&
-                !georgiaA.isChecked() && !georgiaB.isChecked() &&
-                !georgiaD.isChecked()) {
+        if (binding.includedCheck.georgiaRespC.isChecked() && binding.includedCheck.georgiaRespE.isChecked() &&
+                !binding.includedCheck.georgiaRespA.isChecked() && !binding.includedCheck.georgiaRespB.isChecked() &&
+                !binding.includedCheck.georgiaRespD.isChecked()) {
             mCerto++;
         }
     }
@@ -176,7 +164,6 @@ public class MainActivity extends Activity {
     private void zerarRespostas() {
         mCerto = 0;
 
-        LinearLayout componentes = (LinearLayout) findViewById(R.id.linear_componentes);
-        LimparEditsChecksRadios(componentes);
+        LimparEditsChecksRadios(binding.linearComponentes);
     }
 }
